13ec3fa532260f3fc48130326498bbb4294a43df
austin hunting <austin.hunting@hpe.com>
2017-07-25 13:09:47 -0600

    tmp to home

--- builder.bash
+++ builder.bash
@@ -23,7 +23,7 @@ apt-get update && apt-get install -y \
                                 docbook-xsl \
                                 docbook-xml;
 
-cd /tmp;
+cd /home;
 
 git clone https://github.com/FabricAttachedMemory/nvml.git && \
 cd nvml && make dpkg;
@@ -32,34 +32,34 @@ mkdir usr && dpkg-buildpackage -b -rfakeroot -us -uc;
 mv debian/tmp/usr/lib64 usr/lib
 dpkg-buildpackage -b -rfakeroot -us -uc;
 cd .. && cp ./*.deb /deb;
-cd /tmp && rm -rf nvml;
+cd /home && rm -rf nvml;
 
 mkdir dpkg-build;
 cd dpkg-build;
 git clone https://github.com/FabricAttachedMemory/tm-librarian.git && \
 cd tm-librarian && dpkg-buildpackage -b -rfakeroot -us -uc;
-cd /tmp;
+cd /home;
 git clone https://github.com/FabricAttachedMemory/l4fame-node.git && \
 cd l4fame-node && dpkg-buildpackage -b -rfakeroot -us -uc;
-cd /tmp;
+cd /home;
 git clone https://github.com/FabricAttachedMemory/l4fame-manager.git && \
 cd l4fame-manager && dpkg-buildpackage -b -rfakeroot -us -uc;
-cd /tmp;
+cd /home;
 git clone https://github.com/AustinHunting/libfam-atomic.git && \
 cd libfam-atomic && dpkg-buildpackage -b -rfakeroot -us -uc;
-cd /tmp;
+cd /home;
 git clone -b debian https://github.com/keith-packard/Emulation.git && \
 cd Emulation && dpkg-buildpackage -b -rfakeroot -us -uc;
-cd /tmp;
+cd /home;
 git clone -b debian https://github.com/FabricAttachedMemory/tm-hello-world.git && \
 cd tm-hello-world && dpkg-buildpackage -b -rfakeroot -us -uc;
-cd /tmp;
+cd /home;
 git clone -b debian https://github.com/FabricAttachedMemory/tm-libfuse.git && \
 cd tm-libfuse && dpkg-buildpackage -b -rfakeroot -us -uc;
-cd /tmp && cp ./*.deb /deb;
-cd /tmp && rm -rf dpkg-build;
+cd /home && cp ./*.deb /deb;
+cd /home && rm -rf dpkg-build;
 
 git clone https://github.com/FabricAttachedMemory/linux-l4fame.git && \
 cd linux-l4fame && fakeroot make deb-pkg;
-cd /tmp && cp ./*.deb /deb;
+cd /home && cp ./*.deb /deb;
 
