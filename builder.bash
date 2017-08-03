b52302d3ed17b663a5149028d8bf38e864ac9245
austin hunting <austin.hunting@hpe.com>
2017-08-03 16:42:08 -0600

    added check_build_error to locate bad builds

--- builder.bash
+++ builder.bash
@@ -33,66 +33,96 @@ else
     CORES=$(( $(( $((`cat /proc/cpuinfo | grep processor | wc -l`)) + 1 )) / 2 ))
 fi
 
+# Prints ERROR and working directory if a build fails
+check_build_error () {
+    if [ "$?" -ne "0" ]; then
+        echo ""
+        echo "ERROR: Building died at this package"
+        echo "ERROR: `pwd`"
+        echo ""
+    fi
+}
+
 mkdir /deb;
 cd /home;
 
 git clone https://github.com/FabricAttachedMemory/nvml.git && \
 (   ( cd nvml && make -j $CORES dpkg );
     ( cd nvml/dpkgbuild/nvml-* && mkdir usr && dpkg-buildpackage --jobs=$CORES -b -us -uc;
-    mv debian/tmp/usr/lib64 usr/lib && dpkg-buildpackage --jobs=$CORES -b -us -uc;
+    mv debian/tmp/usr/lib64 usr/lib && ( dpkg-buildpackage --jobs=$CORES -b -us -uc;
+        check_build_error; );
     cd .. && cp ./*.deb /deb );
 ) || \
 ( cd nvml && set -- `git pull` && [ "$1" == "Updating" ] && \
     ( make -j $CORES dpkg;
     ( cd dpkgbuild/nvml-* && mkdir usr && dpkg-buildpackage --jobs=$CORES -b -us -uc;
-    mv debian/tmp/usr/lib64 usr/lib && dpkg-buildpackage --jobs=$CORES -b -us -uc;
+    mv debian/tmp/usr/lib64 usr/lib && ( dpkg-buildpackage --jobs=$CORES -b -us -uc;
+        check_build_error; );
     cd .. && cp ./*.deb /deb );
 ) );
 
 git clone https://github.com/FabricAttachedMemory/tm-librarian.git && \
-( cd tm-librarian && dpkg-buildpackage --jobs=$CORES -us -uc ) || \
-( cd tm-librarian && set -- `git pull` && [ "$1" == "Updating" ] && dpkg-buildpackage --jobs=$CORES -us -uc );
+( cd tm-librarian && ( dpkg-buildpackage --jobs=$CORES -us -uc;
+    check_build_error; ); ) || \
+( cd tm-librarian && set -- `git pull` && [ "$1" == "Updating" ] && ( dpkg-buildpackage --jobs=$CORES -us -uc;
+    check_build_error; ); );
 
 git clone https://github.com/FabricAttachedMemory/l4fame-node.git && \
-( cd l4fame-node && dpkg-buildpackage --jobs=$CORES -us -uc ) || \
-( cd l4fame-node && set -- `git pull` && [ "$1" == "Updating" ] && dpkg-buildpackage --jobs=$CORES -us -uc );
+( cd l4fame-node && ( dpkg-buildpackage --jobs=$CORES -us -uc;
+    check_build_error; ); ) || \
+( cd l4fame-node && set -- `git pull` && [ "$1" == "Updating" ] && ( dpkg-buildpackage --jobs=$CORES -us -uc;
+    check_build_error; ); );
 
 git clone https://github.com/FabricAttachedMemory/l4fame-manager.git && \
-( cd l4fame-manager && dpkg-buildpackage --jobs=$CORES -us -uc ) || \
-( cd l4fame-manager && set -- `git pull` && [ "$1" == "Updating" ] && dpkg-buildpackage --jobs=$CORES -us -uc );
+( cd l4fame-manager && ( dpkg-buildpackage --jobs=$CORES -us -uc;
+    check_build_error; ); ) || \
+( cd l4fame-manager && set -- `git pull` && [ "$1" == "Updating" ] && ( dpkg-buildpackage --jobs=$CORES -us -uc;
+    check_build_error; ); );
 
 git clone -b debian https://github.com/FabricAttachedMemory/tm-hello-world.git && \
-( cd tm-hello-world && dpkg-buildpackage --jobs=$CORES -b -us -uc ) || \
-( cd tm-hello-world && set -- `git pull` && [ "$1" == "Updating" ] && dpkg-buildpackage --jobs=$CORES -b -us -uc );
+( cd tm-hello-world && ( dpkg-buildpackage --jobs=$CORES -b -us -uc;
+    check_build_error; ); ) || \
+( cd tm-hello-world && set -- `git pull` && [ "$1" == "Updating" ] && ( dpkg-buildpackage --jobs=$CORES -b -us -uc;
+    check_build_error; ); );
 
 git clone -b debian https://github.com/FabricAttachedMemory/tm-libfuse.git && \
-( cd tm-libfuse && dpkg-buildpackage --jobs=$CORES -b -us -uc ) || \
-( cd tm-libfuse && set -- `git pull` && [ "$1" == "Updating" ] && dpkg-buildpackage --jobs=$CORES -b -us -uc );
+( cd tm-libfuse && ( dpkg-buildpackage --jobs=$CORES -b -us -uc;
+    check_build_error; ); ) || \
+( cd tm-libfuse && set -- `git pull` && [ "$1" == "Updating" ] && ( dpkg-buildpackage --jobs=$CORES -b -us -uc;
+    check_build_error; ); );
 
 git clone https://github.com/FabricAttachedMemory/libfam-atomic.git atomic-deb || \
 ( cd atomic-deb && git pull );
 git clone -b upstream https://github.com/FabricAttachedMemory/libfam-atomic.git && \
-( cp -r atomic-deb/debian libfam-atomic && cd libfam-atomic && dpkg-buildpackage --jobs=$CORES -us -uc ) || \
+( cp -r atomic-deb/debian libfam-atomic && cd libfam-atomic && ( dpkg-buildpackage --jobs=$CORES -us -uc;
+    check_build_error; ); ) || \
 ( cp -r atomic-deb/debian libfam-atomic && cd libfam-atomic && set -- `git pull` && [ "$1" == "Updating" ] && \
-        ( dpkg-buildpackage --jobs=$CORES -us -uc ) );
+        ( dpkg-buildpackage --jobs=$CORES -us -uc;
+            check_build_error; ) );
 
 git clone -b debian https://github.com/keith-packard/Emulation.git Emulation-deb || \
 ( cd Emulation-deb && git pull );
 git clone https://github.com/FabricAttachedMemory/Emulation.git && \
-( cp -r Emulation-deb/debian Emulation && cd Emulation && dpkg-buildpackage --jobs=$CORES -b -us -uc ) || \
+( cp -r Emulation-deb/debian Emulation && cd Emulation && ( dpkg-buildpackage --jobs=$CORES -b -us -uc;
+    check_build_error; ); ) || \
 ( cp -r Emulation-deb/debian Emulation && cd Emulation && set -- `git pull` && [ "$1" == "Updating" ] && \
-        ( dpkg-buildpackage --jobs=$CORES -b -us -uc ) );
+        ( dpkg-buildpackage --jobs=$CORES -b -us -uc;
+            check_build_error; ) );
 
 git clone https://github.com/FabricAttachedMemory/tm-manifesting.git && \
-( cd tm-manifesting && dpkg-buildpackage --jobs=$CORES -b -us -uc ) || \
-( cd tm-manifesting && set -- `git pull` && [ "$1" == "Updating" ] && dpkg-buildpackage --jobs=$CORES -b -us -uc );
+( cd tm-manifesting && ( dpkg-buildpackage --jobs=$CORES -b -us -uc;
+    check_build_error; ); ) || \
+( cd tm-manifesting && set -- `git pull` && [ "$1" == "Updating" ] && ( dpkg-buildpackage --jobs=$CORES -b -us -uc;
+    check_build_error; ); );
 
 cp /home/*.deb /deb;
 
 
 git clone https://github.com/FabricAttachedMemory/linux-l4fame.git && \
-( cd linux-l4fame && make -j $CORES deb-pkg ) || \
-( cd linux-l4fame && set -- `git pull` && [ "$1" == "Updating" ] && make -j $CORES deb-pkg );
+( cd linux-l4fame && ( make -j $CORES deb-pkg;
+    check_build_error; ); ) || \
+( cd linux-l4fame && set -- `git pull` && [ "$1" == "Updating" ] && ( make -j $CORES deb-pkg;
+    check_build_error; ); );
 
 cp /home/*.deb /deb;
 
