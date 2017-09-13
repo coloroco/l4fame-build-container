5afcae4c6033144f0a94c2841cf5cc724d7185a8
austin hunting <austin.hunting@hpe.com>
2017-09-13 16:45:00 -0600

    moved gbp out of function

--- builder.bash
+++ builder.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# install updates
+# Install updates
 apt update && \
 apt upgrade -y && \
 apt install -y git-buildpackage;
@@ -14,7 +14,6 @@ ignore-new = True
 
 [buildpackage]
 export-dir = /build-area/
-tarball-dir = /tarballs/
 
 [git-import-orig]
 dch = False
@@ -27,26 +26,10 @@ EOF
 
 # Checks for needed build packages and installs them
 run_update () {
+    ( git checkout debian );
     ( ls debian &>/dev/null ) && \
     ( ( dpkg-checkbuilddeps &>/dev/null ) || \
     ( echo "y" | mk-build-deps -i -r ) )
-}
-
-# Builds packages
-run_builder () {
-    ( git checkout debian );
-    ( run_update );
-    ( gbp buildpackage "$*" );
-}
-
-# prints an error message if a build fails
-check_failure () {
-    if [ "$?" ]; then
-        echo "BUILD FAILURE"
-        pwd
-        echo "Trying again"
-        gbp buildpackage "$*"
-    fi
 }
 
 # Builds a new debian/rules file for nvml
@@ -77,7 +60,6 @@ echo -e "$rule" > /tmp/rules
 chmod +x /tmp/rules
 }
 
-
 # If user sets "-e cores=number_of_cores" use that many cores to compile
 if [ "$cores" ]; then
     CORES=$(( $cores ))
@@ -99,48 +81,48 @@ set_config_files;
 
 # git checkout debian && apply new rules file && --git-prebuild='mv /tmp/rules debian/rules'
 git clone https://github.com/FabricAttachedMemory/nvml.git && \
-    ( cd nvml && git checkout upstream && fix_nvml_rules && run_builder --git-prebuild='mv /tmp/rules debian/rules' ) || \
-    ( cd nvml && git checkout upstream && fix_nvml_rules && set -- `git pull` && [ "$1" == "Updating" ] && run_builder --git-prebuild='mv /tmp/rules debian/rules' )
+    ( cd nvml && git checkout upstream && fix_nvml_rules && run_update && gbp buildpackage --git-prebuild='mv /tmp/rules debian/rules' ) || \
+    ( cd nvml && git checkout upstream && fix_nvml_rules && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage --git-prebuild='mv /tmp/rules debian/rules' )
 
-# git checkout upstream && git checkout debian && run_builder
-git clone -b debian https://github.com/keith-packard/tm-librarian.git && \
-    ( cd tm-librarian && git checkout upstream && run_builder ) || \
-    ( cd tm-librarian && git checkout upstream && set -- `git pull` && [ "$1" == "Updating" ] && run_builder )
+# run_update && gbp buildpackage --git-upstream-branch=master --git-upstream-tree=branch
+git clone https://github.com/AustinHunting/tm-librarian.git && \
+    ( cd tm-librarian && run_update && gbp buildpackage --git-upstream-branch=master --git-upstream-tree=branch ) || \
+    ( cd tm-librarian && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage --git-upstream-branch=master --git-upstream-tree=branch )
 
-# run_builder && gbp buildpackage --git-upstream-branch=master --git-upstream-tree=branch
+# run_update && gbp buildpackage && gbp buildpackage --git-upstream-branch=master --git-upstream-tree=branch
 git clone https://github.com/keith-packard/tm-manifesting.git && \
-    ( cd tm-manifesting && run_builder --git-upstream-branch=master --git-upstream-tree=branch ) || \
-    ( cd tm-manifesting && set -- `git pull` && [ "$1" == "Updating" ] && run_builder --git-upstream-branch=master --git-upstream-tree=branch )
+    ( cd tm-manifesting && run_update && gbp buildpackage --git-upstream-branch=master --git-upstream-tree=branch ) || \
+    ( cd tm-manifesting && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage --git-upstream-branch=master --git-upstream-tree=branch )
 
-# git checkout debian && run_builder
+# git checkout debian && run_update && gbp buildpackage
 git clone https://github.com/FabricAttachedMemory/l4fame-node.git && \
-    ( cd l4fame-node && run_builder ) || \
-    ( cd l4fame-node && set -- `git pull` && [ "$1" == "Updating" ] && run_builder )
+    ( cd l4fame-node && run_update && gbp buildpackage ) || \
+    ( cd l4fame-node && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage )
 
-# git checkout debian && run_builder
+# git checkout debian && run_update && gbp buildpackage
 git clone https://github.com/FabricAttachedMemory/l4fame-manager.git && \
-    ( cd l4fame-manager && run_builder ) || \
-    ( cd l4fame-manager && set -- `git pull` && [ "$1" == "Updating" ] && run_builder )
+    ( cd l4fame-manager && run_update && gbp buildpackage ) || \
+    ( cd l4fame-manager && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage )
 
-# git checkout debian && run_builder
+# git checkout debian && run_update && gbp buildpackage
 git clone https://github.com/FabricAttachedMemory/tm-hello-world.git && \
-    ( cd tm-hello-world && run_builder ) || \
-    ( cd tm-hello-world && set -- `git pull` && [ "$1" == "Updating" ] && run_builder )
+    ( cd tm-hello-world && run_update && gbp buildpackage ) || \
+    ( cd tm-hello-world && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage )
 
-# git checkout upstream && git checkout debian && run_builder
+# git checkout upstream && git checkout debian && run_update && gbp buildpackage
 git clone https://github.com/FabricAttachedMemory/tm-libfuse.git && \
-    ( cd tm-libfuse && git checkout upstream && run_builder ) || \
-    ( cd tm-libfuse && git checkout upstream && set -- `git pull` && [ "$1" == "Updating" ] && run_builder )
+    ( cd tm-libfuse && git checkout upstream && run_update && gbp buildpackage ) || \
+    ( cd tm-libfuse && git checkout upstream && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage )
 
 # git checkout upstream && git checkout debian && gbp buildpackage --git-upstream-tree=branch
 git clone https://github.com/FabricAttachedMemory/libfam-atomic.git && \
-    ( cd libfam-atomic && git checkout upstream && run_builder --git-upstream-tree=branch ) || \
-    ( cd libfam-atomic && git checkout upstream && set -- `git pull` && [ "$1" == "Updating" ] && run_builder --git-upstream-tree=branch )
+    ( cd libfam-atomic && git checkout upstream && run_update && gbp buildpackage --git-upstream-tree=branch ) || \
+    ( cd libfam-atomic && git checkout upstream && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage --git-upstream-tree=branch )
 
 # git checkout debian && gbp buildpackage --git-upstream-branch=master
 git clone https://github.com/FabricAttachedMemory/Emulation.git && \
-    ( cd Emulation && run_builder --git-upstream-branch=master ) || \
-    ( cd Emulation && set -- `git pull` && [ "$1" == "Updating" ] && run_builder --git-upstream-branch=master )
+    ( cd Emulation && run_update && gbp buildpackage --git-upstream-branch=master ) || \
+    ( cd Emulation && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage --git-upstream-branch=master )
 
 # copy all .debs to external deb folder
 cp /build-area/*.deb /deb;
