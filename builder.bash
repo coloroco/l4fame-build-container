fecdb2a2fa7b91fa753f6f523e7850d1b5774f09
austin hunting <austin.hunting@hpe.com>
2017-09-25 17:46:02 -0600

    cleaned up pathways from 4 to three

--- builder.bash
+++ builder.bash
@@ -32,6 +32,7 @@ EOF
 
 # Checks for needed build packages and installs them
 run_update () {
+    ( git checkout upstream );
     ( git checkout debian );
     ( ls debian &>/dev/null ) && \
     ( ( dpkg-checkbuilddeps &>/dev/null ) || \
@@ -102,58 +103,59 @@ cd /build;
 set_config_files;
 
 # git checkout debian && apply new rules file && --git-prebuild='mv /tmp/rules debian/rules'
-git clone https://github.com/FabricAttachedMemory/nvml.git && \
-    ( cd nvml && git checkout upstream && fix_nvml_rules && run_update && gbp buildpackage --git-prebuild='mv /tmp/rules debian/rules' ) || \
-    ( cd nvml && git checkout upstream && fix_nvml_rules && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage --git-prebuild='mv /tmp/rules debian/rules' )
+git clone https://github.com/FabricAttachedMemory/nvml.git || \
+    ( cd nvml && git checkout upstream && fix_nvml_rules && set -- `git pull` && [ "$1" == "Updating" ] ) && \
+    ( cd nvml && git checkout upstream && fix_nvml_rules && run_update && gbp buildpackage --git-prebuild='mv /tmp/rules debian/rules' );
 
 # clone debian && checkout upstream run_update && gbp buildpackage
-git clone -b debian https://github.com/FabricAttachedMemory/tm-librarian.git && \
-    ( cd tm-librarian && git checkout upstream && run_update && gbp buildpackage ) || \
-    ( cd tm-librarian && git checkout upstream && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage )
+git clone -b debian https://github.com/FabricAttachedMemory/tm-librarian.git || \
+    ( cd tm-librarian && git checkout upstream && set -- `git pull` && [ "$1" == "Updating" ] ) && \
+    ( cd tm-librarian && git checkout upstream && run_update && gbp buildpackage );
 
 # run_update && gbp buildpackage && gbp buildpackage --git-upstream-branch=master --git-upstream-tree=branch
-git clone https://github.com/keith-packard/tm-manifesting.git && \
-    ( cd tm-manifesting && run_update && gbp buildpackage --git-upstream-branch=master --git-upstream-tree=branch ) || \
-    ( cd tm-manifesting && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage --git-upstream-branch=master --git-upstream-tree=branch )
+git clone https://github.com/keith-packard/tm-manifesting.git || \
+    ( cd tm-manifesting && set -- `git pull` && [ "$1" == "Updating" ] ) && \
+    ( cd tm-manifesting && run_update && gbp buildpackage --git-upstream-branch=master --git-upstream-tree=branch );
 
 # git checkout debian && run_update && gbp buildpackage
-git clone https://github.com/FabricAttachedMemory/l4fame-node.git && \
-    ( cd l4fame-node && run_update && gbp buildpackage ) || \
-    ( cd l4fame-node && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage )
+git clone https://github.com/FabricAttachedMemory/l4fame-node.git || \
+    ( cd l4fame-node && set -- `git pull` && [ "$1" == "Updating" ] ) && \
+    ( cd l4fame-node && run_update && gbp buildpackage );
 
 # git checkout debian && run_update && gbp buildpackage
-git clone https://github.com/FabricAttachedMemory/l4fame-manager.git && \
-    ( cd l4fame-manager && run_update && gbp buildpackage ) || \
-    ( cd l4fame-manager && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage )
+git clone https://github.com/FabricAttachedMemory/l4fame-manager.git || \
+    ( cd l4fame-manager && set -- `git pull` && [ "$1" == "Updating" ] ) && \
+    ( cd l4fame-manager && run_update && gbp buildpackage );
 
 # git checkout debian && run_update && gbp buildpackage
-git clone https://github.com/FabricAttachedMemory/tm-hello-world.git && \
-    ( cd tm-hello-world && run_update && gbp buildpackage ) || \
-    ( cd tm-hello-world && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage )
+git clone https://github.com/FabricAttachedMemory/tm-hello-world.git || \
+    ( cd tm-hello-world && set -- `git pull` && [ "$1" == "Updating" ] ) && \
+    ( cd tm-hello-world && run_update && gbp buildpackage );
 
 # git checkout upstream && git checkout debian && run_update && gbp buildpackage
-git clone https://github.com/FabricAttachedMemory/tm-libfuse.git && \
-    ( cd tm-libfuse && git checkout upstream && run_update && gbp buildpackage ) || \
-    ( cd tm-libfuse && git checkout upstream && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage )
+git clone https://github.com/FabricAttachedMemory/tm-libfuse.git || \
+    ( cd tm-libfuse && set -- `git pull` && [ "$1" == "Updating" ] ) && \
+    ( cd tm-libfuse && run_update && gbp buildpackage );
 
 # git checkout upstream && git checkout debian && gbp buildpackage --git-upstream-tree=branch
-git clone https://github.com/FabricAttachedMemory/libfam-atomic.git && \
-    ( cd libfam-atomic && git checkout upstream && run_update && gbp buildpackage --git-upstream-tree=branch ) || \
-    ( cd libfam-atomic && git checkout upstream && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage --git-upstream-tree=branch )
+git clone https://github.com/FabricAttachedMemory/libfam-atomic.git || \
+    ( cd libfam-atomic && set -- `git pull` && [ "$1" == "Updating" ] ) && \
+    ( cd libfam-atomic && run_update && gbp buildpackage --git-upstream-tree=branch );
 
 # git checkout debian && gbp buildpackage --git-upstream-branch=master
-git clone https://github.com/FabricAttachedMemory/Emulation.git && \
-    ( cd Emulation && run_update && gbp buildpackage --git-upstream-branch=master ) || \
-    ( cd Emulation && set -- `git pull` && [ "$1" == "Updating" ] && run_update && gbp buildpackage --git-upstream-branch=master )
+git clone https://github.com/FabricAttachedMemory/Emulation.git || \
+    ( cd Emulation && set -- `git pull` && [ "$1" == "Updating" ] ) && \
+    ( cd Emulation && run_update && gbp buildpackage --git-upstream-branch=master );
 
 # copy all .debs to external deb folder
 cp /gbp-build-area/*.deb /deb;
 
 # Old pathway
-git clone https://github.com/FabricAttachedMemory/linux-l4fame.git && \
-    ( cd linux-l4fame && make -j $CORES deb-pkg ) || \
-    ( cd linux-l4fame && set -- `git pull` && [ "$1" == "Updating" ] && make -j $CORES deb-pkg );
+git clone https://github.com/FabricAttachedMemory/linux-l4fame.git || \
+    ( cd linux-l4fame && set -- `git pull` && [ "$1" == "Updating" ] ) && \
+    ( cd linux-l4fame && make -j $CORES deb-pkg );
 cp /build/*.deb /deb;
+
 
 # Change into the chroot and run builder.bash
 set -- `basename $0`
