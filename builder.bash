9d72b233c153c3f892d3edff022e5b04db272a96
austin hunting <austin.hunting@hpe.com>
2017-09-27 17:01:43 -0600

    moved from the double build system to the tagging system, large structure changes

--- builder.bash
+++ builder.bash
@@ -2,17 +2,19 @@
 apt-get update && apt-get upgrade -y;
 apt-get install -y git-buildpackage;                            # Needed for all packages
 apt-get install -y libssl-dev bc pkg-config build-essential;    # Needed for the kernel
-
+# Check if we're running in docker or a chroot
 if [ $(ls -di / | cut -d ' ' -f 1) == "2" ]; then
-    # Install packages in the docker container
+    # Only needed in the docker container
     apt-get install -y debootstrap qemu qemu-user-static;
 else
-    # Install packages in the arm64 chroot
+    # Only needed in the arm64 chroot
     apt-get install -y linux-image-arm64;
 fi
 
+
 # Sets the configuration files for devbuilder and gbp
 set_config_files () {
+# gbp configuration file
 cat <<EOF > $HOME/.gbp.conf
 [DEFAULT]
 cleaner = fakeroot debian/rules clean
@@ -21,16 +23,29 @@ ignore-new = True
 [buildpackage]
 export-dir = /gbp-build-area/
 
+EOF
+# Insert a postbuild command into the middle of the gbp configuration file
+# This indicates to the arm64 chroot which repositories need to be built
+if [ $(ls -di / | cut -d ' ' -f 1) == "2" ]; then
+    # In docker, mark repositories to be built
+    echo "postbuild=touch ../\$(basename \`pwd\`)-update" >> $HOME/.gbp.conf
+else
+    # In chroot, mark built repositories
+    echo "postbuild=rm ../\$(basename \`pwd\`)-update" >> $HOME/.gbp.conf
+fi
+cat <<EOF >> $HOME/.gbp.conf
 [git-import-orig]
 dch = False
 EOF
 
+# devbuilder configuration file
 cat <<EOF > $HOME/.devscripts
 DEBUILD_DPKG_BUILDPACKAGE_OPTS="-us -uc -i -b --jobs=$CORES"
 EOF
 }
 
-# Checks for needed build packages and installs them
+
+# Check for prerequisite build packages, and install them
 run_update () {
     ( git checkout upstream );
     ( git checkout debian );
@@ -38,6 +53,7 @@ run_update () {
     ( ( dpkg-checkbuilddeps &>/dev/null ) || \
     ( echo "y" | mk-build-deps -i -r ) )
 }
+
 
 # Builds a new debian/rules file for nvml
 fix_nvml_rules () {
@@ -67,90 +83,108 @@ echo -e "$rule" > /tmp/rules
 chmod +x /tmp/rules
 }
 
-# If user sets "-e cores=number_of_cores" use that many cores to compile
+
+# Sets $path to the path if a build is required or "" if no build is required
+# Call with a github repository URL, example:
+# get_update_path https://github.com/FabricAttachedMemory/l4fame-build-container.git
+get_update_path () {
+    # Get a path from the git URL
+    path="/build/"`echo "$1" | cut -d '/' -f 5 | cut -d '.' -f 1`;
+    # Check if we're running in docker or a chroot
+    if [ $(ls -di / | cut -d ' ' -f 1) == "2" ]; then
+        # Check if the repository needs to be cloned, then clone
+        ls $path &>/dev/null;
+        if [ "$?" != "0" ]; then
+            git clone "$1";
+        else
+            # Check if there is an update, then update
+            cd $path;
+            set -- `git pull`;
+            if [ "$1" != "Updating" ]; then
+                path="";
+            fi
+        fi
+    else
+        # Check if docker marked the repository as needing a rebuild
+        ls $path"-update" &>/dev/null;
+        if [ "$?" != "0" ]; then
+            path="";
+        fi
+    fi
+}
+
+
+# If user sets "-e cores=number_of_cores" use that many cores when compiling
 if [ "$cores" ]; then
     CORES=$(( $cores ))
-# Set $CORES to half the cpu cores.
+# Otherwise set $CORES to half the cpu cores.
 else
     CORES=$(( ( `nproc` + 1 ) / 2 ))
 fi
 
-# Check if running in a chroot
+# Check if we're running in docker or a chroot
 if [ $(ls -di / | cut -d ' ' -f 1) == "2" ]; then
-    # build chroot if none exists
+    # Build an arm64 chroot if none exists
     ( ls /arm64 &>/dev/null ) || qemu-debootstrap --arch=arm64 unstable /arm64/jessie http://httpredir.debian.org/debian;
-    # mount /deb and /build so we can get at them from inside the chroot
+    # Make the directories that gbp will download repositories to and build in
     mkdir -p /deb;
     mkdir -p /build;
     mkdir -p /deb/arm64;
     mkdir -p /arm64/jessie/deb;
     mkdir -p /arm64/jessie/build;
+    # Mount /deb and /build so we can get at them from inside the chroot
     mount --bind /deb/arm64 /arm64/jessie/deb;
-    # mount --bind /build /arm64/jessie/build;
-    # Copy builder.bash into the chroot
+    mount --bind /build /arm64/jessie/build;
+    # Copy this script into the chroot
     cp "$0" /arm64/jessie
 fi
 
+# Change into build directory and set the configuration files
 cd /build;
 set_config_files;
 
-# git checkout debian && apply new rules file && --git-prebuild='mv /tmp/rules debian/rules'
-git clone https://github.com/FabricAttachedMemory/nvml.git || \
-    ( cd nvml && git checkout upstream && fix_nvml_rules && set -- `git pull` && [ "$1" == "Updating" ] ) && \
-    ( cd nvml && git checkout upstream && fix_nvml_rules && run_update && gbp buildpackage --git-prebuild='mv /tmp/rules debian/rules' );
 
-# clone debian && checkout upstream run_update && gbp buildpackage
-git clone https://github.com/FabricAttachedMemory/tm-librarian.git || \
-    ( cd tm-librarian && set -- `git pull` && [ "$1" == "Updating" ] ) && \
-    ( cd tm-librarian && run_update && gbp buildpackage );
+fix_nvml_rules;
+get_update_path https://github.com/FabricAttachedMemory/nvml.git;
+( cd $path && run_update && gbp buildpackage --git-prebuild='mv /tmp/rules debian/rules' );
 
-# run_update && gbp buildpackage && gbp buildpackage --git-upstream-branch=master --git-upstream-tree=branch
-git clone https://github.com/keith-packard/tm-manifesting.git || \
-    ( cd tm-manifesting && set -- `git pull` && [ "$1" == "Updating" ] ) && \
-    ( cd tm-manifesting && run_update && gbp buildpackage --git-upstream-branch=master --git-upstream-tree=branch );
+get_update_path https://github.com/FabricAttachedMemory/tm-librarian.git;
+( cd $path && run_update && gbp buildpackage );
 
-# git checkout debian && run_update && gbp buildpackage
-git clone https://github.com/FabricAttachedMemory/l4fame-node.git || \
-    ( cd l4fame-node && set -- `git pull` && [ "$1" == "Updating" ] ) && \
-    ( cd l4fame-node && run_update && gbp buildpackage );
+get_update_path https://github.com/keith-packard/tm-manifesting.git;
+( cd $path && run_update && gbp buildpackage --git-upstream-branch=master --git-upstream-tree=branch );
 
-# git checkout debian && run_update && gbp buildpackage
-git clone https://github.com/FabricAttachedMemory/l4fame-manager.git || \
-    ( cd l4fame-manager && set -- `git pull` && [ "$1" == "Updating" ] ) && \
-    ( cd l4fame-manager && run_update && gbp buildpackage );
+get_update_path https://github.com/FabricAttachedMemory/l4fame-node.git;
+( cd $path && run_update && gbp buildpackage );
 
-# git checkout debian && run_update && gbp buildpackage
-git clone https://github.com/FabricAttachedMemory/tm-hello-world.git || \
-    ( cd tm-hello-world && set -- `git pull` && [ "$1" == "Updating" ] ) && \
-    ( cd tm-hello-world && run_update && gbp buildpackage );
+get_update_path https://github.com/FabricAttachedMemory/l4fame-manager.git;
+( cd $path && run_update && gbp buildpackage );
 
-# git checkout upstream && git checkout debian && run_update && gbp buildpackage
-git clone https://github.com/FabricAttachedMemory/tm-libfuse.git || \
-    ( cd tm-libfuse && set -- `git pull` && [ "$1" == "Updating" ] ) && \
-    ( cd tm-libfuse && run_update && gbp buildpackage );
+get_update_path https://github.com/FabricAttachedMemory/tm-hello-world.git;
+( cd $path && run_update && gbp buildpackage );
 
-# git checkout upstream && git checkout debian && gbp buildpackage --git-upstream-tree=branch
-git clone https://github.com/FabricAttachedMemory/libfam-atomic.git || \
-    ( cd libfam-atomic && set -- `git pull` && [ "$1" == "Updating" ] ) && \
-    ( cd libfam-atomic && run_update && gbp buildpackage --git-upstream-tree=branch );
+get_update_path https://github.com/FabricAttachedMemory/tm-libfuse.git;
+( cd $path && run_update && gbp buildpackage );
 
-# git checkout debian && gbp buildpackage --git-upstream-branch=master
-git clone https://github.com/FabricAttachedMemory/Emulation.git || \
-    ( cd Emulation && set -- `git pull` && [ "$1" == "Updating" ] ) && \
-    ( cd Emulation && run_update && gbp buildpackage --git-upstream-branch=master );
+get_update_path https://github.com/FabricAttachedMemory/libfam-atomic.git;
+( cd $path && run_update && gbp buildpackage --git-upstream-tree=branch );
 
-# copy all .debs to external deb folder
+get_update_path https://github.com/FabricAttachedMemory/Emulation.git;
+( cd $path && run_update && gbp buildpackage --git-upstream-branch=master );
+
+# Copy all the built .deb's to the external deb folder
 cp /gbp-build-area/*.deb /deb;
 
-# Don't build for arm64, .config file is unset
+
+# Don't build the kernel for arm64, .config file is unset, never completes
 if [ $(ls -di / | cut -d ' ' -f 1) == "2" ]; then
-    git clone https://github.com/FabricAttachedMemory/linux-l4fame.git || \
-        ( cd linux-l4fame && set -- `git pull` && [ "$1" == "Updating" ] ) && \
-        ( cd linux-l4fame && make -j $CORES deb-pkg );
+    get_update_path https://github.com/FabricAttachedMemory/linux-l4fame.git;
+    ( cd $path && make -j $CORES deb-pkg );
     cp /build/*.deb /deb;
 fi
 
-# Change into the chroot and run builder.bash
+
+# Change into the chroot and run this script
 set -- `basename $0`
 if [ $(ls -di / | cut -d ' ' -f 1) == "2" ]; then
     chroot /arm64/jessie "/$1" 'cores=$CORES' 'http_proxy=$http_proxy' 'https_proxy=$https_proxy'
